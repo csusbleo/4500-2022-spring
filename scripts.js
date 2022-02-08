2a8f59a6558e2060cdb3adc73e499170220e27f1
leoli <yli@csusb.edu>
2022-02-08 13:56:30 -0800

    Update

--- scripts.js
+++ scripts.js
@@ -32,19 +32,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-function showTime() {
-            let time = new Date();
-            let hour = time.getHours();
-            let min = time.getMinutes();
-            let sec = time.getSeconds();
-
-            hour = hour < 10 ? "0" + hour : hour;
-            min = min < 10 ? "0" + min : min;
-            sec = sec < 10 ? "0" + sec : sec;
-
-            let currentTime = hour + ":" + min + ":" + sec;
-
-            document.getElementById("clock")
-                    .innerHTML = currentTime;
-          }
