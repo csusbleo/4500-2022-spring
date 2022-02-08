cd29da34a3c7616eb362da47e06615d438f88a25
leoli <yli@csusb.edu>
2022-02-08 12:42:48 -0800

    Update index.php

--- index.php
+++ index.php
@@ -77,14 +77,6 @@
             <section class="resume-section" id="experience">
                 <div class="resume-section-content">
                     <h2 class="mb-5">Experience</h2>
-                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">Senior Web Developer</h3>
-                            <div class="subheading mb-3">Intelitec Solutions</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
-                    </div>
                     <?php
                       $json = file_get_contents('experience.json');
                       $json_data = json_decode($json,true);
@@ -98,6 +90,7 @@
                         echo '<p>'.$experience['description'].'</p>';
                         echo '</div>';
                         echo '<div class="flex-shrink-0"><span class="text-primary">'.$experience['time'].'</span></div>';
+                        echo '</div>';
                       }
 
                     ?>
