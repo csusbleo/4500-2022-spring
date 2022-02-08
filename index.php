2263da0130ed4aebb1d328a0b67df4b3e321eb1d
leoli <yli@csusb.edu>
2022-02-08 12:56:33 -0800

    Update index.php

--- index.php
+++ index.php
@@ -229,23 +229,12 @@
             let hour = time.getHours();
             let min = time.getMinutes();
             let sec = time.getSeconds();
-            am_pm = "AM";
-
-            if (hour > 12) {
-                hour -= 12;
-                am_pm = "PM";
-            }
-            if (hour == 0) {
-                hr = 12;
-                am_pm = "AM";
-            }
 
             hour = hour < 10 ? "0" + hour : hour;
             min = min < 10 ? "0" + min : min;
             sec = sec < 10 ? "0" + sec : sec;
 
-            let currentTime = hour + ":"
-                    + min + ":" + sec + am_pm;
+            let currentTime = hour + ":" + min + ":" + sec;
 
             document.getElementById("clock")
                     .innerHTML = currentTime;
