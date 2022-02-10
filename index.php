e79c0ab5650081c80877cb58df1cf7dfdbc3e65b
leoli <yli@csusb.edu>
2022-02-10 09:58:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
       <div id="about" class="content-item">
         <div class="vertical-center">
           <h1><?php echo $json_data['first-name']; ?><span class="text-primary"><?php echo $json_data['last-name']; ?></span></h1>
-          <div class="subheading">
+          <div class="subheading mb-3">
             <?php echo $json_data['address']; ?> · <?php echo $json_data['phone']; ?> ·
             <a href="<?php echo $json_data['email']; ?>"><?php echo $json_data['email']; ?></a>
           </div>
