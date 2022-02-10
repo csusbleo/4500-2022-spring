732813cdbaf08bba414c4a449cf3ede493deb504
leoli <yli@csusb.edu>
2022-02-10 09:52:45 -0800

    update

--- index.php
+++ index.php
@@ -29,11 +29,12 @@
     <div data-bs-offset="0" tabindex="0">
       <div id="about" class="content-item">
         <div class="vertical-center">
-          <h1>Firstname Lastname</h1>
+          <h1><?php echo $json_data['first-name']; ?><span class="text-primary"><?php echo $json_data['last-name']; ?></span></h1>
           <div class="subheading">
-            5500 University Pkwy, San Bernardino, CA 92407 · (123)456-7890
+            <?php echo $json_data['address']; ?> · <?php echo $json_data['phone']; ?> ·
+            <a href="<?php echo $json_data['email']; ?>"><?php echo $json_data['email']; ?></a>
           </div>
-          <p>...</p>
+          <p><?php echo $json_data['introduction']; ?></p>
         </div>
       </div>
       <hr>
